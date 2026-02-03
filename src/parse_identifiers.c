545f5d09833e47a428c56a0fbe809b0976dc4f4d
NatuAls <nahuelalesso19@gmail.com>
2026-02-03 14:19:49 +0100

    feat(textures): implement XPM texture loading and rendering
    Added full support for wall textures:
    - Parsing of texture identifiers (NO, SO, WE, EA)
    - XPM loading through MLX
    - Texture assignment during raycasting
    - Updated rendering pipeline to sample correct texture pixels
    - Added texture assets to project structure

---
+++ src/parse_identifiers.c
@@ -0,0 +1,79 @@
+#include "cub3d.h"
+
+int	parse_tex_path(char **identifier, char *line)
+{
+	int		i;
+
+	i = 2;
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	*identifier = ft_substr(line, i, ft_strlen(line) - i);
+	if (!*identifier)
+		return (ft_error_sys("malloc"));
+	return (1);
+}
+
+int	parse_line_content(t_game *game, char *line)
+{
+	int	res;
+
+	if (!ft_strncmp(line, "NO", 2) && !game->tex.path_no)
+		res = parse_tex_path(&game->tex.path_no, line);
+	else if (!ft_strncmp(line, "SO", 2) && !game->tex.path_so)
+		res = parse_tex_path(&game->tex.path_so, line);
+	else if (!ft_strncmp(line, "WE", 2) && !game->tex.path_we)
+		res = parse_tex_path(&game->tex.path_we, line);
+	else if (!ft_strncmp(line, "EA", 2) && !game->tex.path_ea)
+		res = parse_tex_path(&game->tex.path_ea, line);
+	else if (line[0] == 'C' && game->ceiling.hex == -1)
+		res = parse_color(&game->ceiling, line);
+	else if (line[0] == 'F' && game->floor.hex == -1)
+		res = parse_color(&game->floor, line);
+	else
+		return (ft_error_msg("Invalid or duplicate identifier"));
+	return (res);
+}
+
+int	analyze_line(t_game *game, char *line)
+{
+	char	*tmp;
+
+	tmp = ft_strtrim(line, " \n\t");
+	free(line);
+	if (!tmp)
+		return (-1);
+	if (tmp[0] == '\0')
+	{
+		free(tmp);
+		return (0);
+	}
+	if (parse_line_content(game, tmp))
+	{
+		free(tmp);
+		return (1);
+	}
+	free(tmp);
+	return (-1);
+}
+
+int	parse_identifiers(t_game *game, int fd)
+{
+	char	*line;
+	int		i;
+	int		res;
+
+	i = 0;
+	while (i < 6)
+	{
+		line = get_next_line(fd, 0);
+		if (!line)
+			break ;
+		res = analyze_line(game, line);
+		if (res == -1)
+			return (0);
+		i += res;
+	}
+	if (i < 6)
+		return (ft_error_msg("Missing identifiers or EOF"));
+	return (1);
+}
